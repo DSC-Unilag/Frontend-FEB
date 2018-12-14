c8e62f200edd3c5ad045be1fc078b7fbd7a5dcf5
Olamide <aboyejiolamide15@gmail.com>
2018-12-14 19:25:12 +0100

    Day12 Completed

--- aboyeji.olamide-aboyejiolamide15/js/schema.js
+++ aboyeji.olamide-aboyejiolamide15/js/schema.js
@@ -1,7 +1,17 @@
 var button = document.querySelector("button");
 var closeButton = document.getElementById("closeButton");
 var newTodoBlock = document.getElementsByClassName("newTodoBlock")[0];
+var todoText = document.getElementById("todoText");
 var form = document.querySelector("form");
+var allUL = document.getElementById("allUL");
+var personalUL = document.getElementById("personalUL");
+var homeUL = document.getElementById("homeUL");
+var workUL = document.getElementById("workUL");
+var categorySelected = document.getElementById("todoCategory");
+//Day 12 task
+var todoDeadlineDate = document.querySelector("input[type=date]");
+var todoDeadlineTime =  document.querySelector("input[type=time]");
+//
 button.addEventListener("click", addTodo);
 form.addEventListener("submit", formSubmit);
 closeButton.addEventListener("click", function(){
@@ -16,5 +26,18 @@ function formSubmit(e){
     e.preventDefault();
     newTodoBlock.classList.remove("newTodoBlockActive");
     button.style.display = "initial";
+    // console.log(todoText.value);
     //More codes to come
+    var newTodo = document.createElement("li");
+    var textNode = document.createTextNode(todoText.value);
+    newTodo.appendChild(textNode);
+    allUL.appendChild(newTodo); 
+    if(categorySelected.value === "personalTodo"){
+            personalUL.appendChild(newTodo.cloneNode(true));
+        } else if(categorySelected.value === "homeTodo"){
+            homeUL.appendChild(newTodo.cloneNode(true));
+        } else if(categorySelected.value === "workTodo"){
+            workUL.appendChild(newTodo.cloneNode(true));
+        }
 }
+
