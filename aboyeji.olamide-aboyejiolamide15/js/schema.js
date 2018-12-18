1ac88116355274ed9c8f915fc3b2fc6e5bb52e48
Olamide <aboyejiolamide15@gmail.com>
2018-12-18 21:16:38 +0100

    Day15 task completed

--- aboyeji.olamide-aboyejiolamide15/js/schema.js
+++ aboyeji.olamide-aboyejiolamide15/js/schema.js
@@ -1,13 +1,16 @@
+//Declare all variables
 var button = document.querySelector("button");
 var closeButton = document.getElementById("closeButton");
 var newTodoBlock = document.getElementsByClassName("newTodoBlock")[0];
 var todoText = document.getElementById("todoText");
 var form = document.querySelector("form");
-var allUL = document.getElementById("allUL");
-var personalUL = document.getElementById("personalUL");
-var homeUL = document.getElementById("homeUL");
-var workUL = document.getElementById("workUL");
 var categorySelected = document.getElementById("todoCategory");
+var categoryHeader = document.getElementsByClassName("todoCategoryHeader")[0];
+var all = document.getElementById("allNav");
+var personal = document.getElementById("personalNav");
+var work = document.getElementById("workNav");
+var home = document.getElementById("homeNav");
+var TodosList = document.getElementById("TodosList");
 //Day 12 task
 var todoDeadlineDate = document.querySelector("input[type=date]");
 var todoDeadlineTime =  document.querySelector("input[type=time]");
@@ -22,22 +25,69 @@ function addTodo(){
     newTodoBlock.classList.add("newTodoBlockActive");
     button.style.display = "none";
 }
+
 function formSubmit(e){
     e.preventDefault();
     newTodoBlock.classList.remove("newTodoBlockActive");
     button.style.display = "initial";
-    // console.log(todoText.value);
-    //More codes to come
     var newTodo = document.createElement("li");
-    var textNode = document.createTextNode(todoText.value);
-    newTodo.appendChild(textNode);
-    allUL.appendChild(newTodo); 
-    if(categorySelected.value === "personalTodo"){
-            personalUL.appendChild(newTodo.cloneNode(true));
-        } else if(categorySelected.value === "homeTodo"){
-            homeUL.appendChild(newTodo.cloneNode(true));
-        } else if(categorySelected.value === "workTodo"){
-            workUL.appendChild(newTodo.cloneNode(true));
-        }
+    newTodo.innerHTML = todoText.value + "<span class=hide " + categorySelected.value + "></span>" + "<span title='Remove Todo' class='delete'>&#128465;</span><br>" + 
+    todoDeadlineDate.value + " "+ todoDeadlineTime.value;
+    if (categorySelected.value === "personal"){
+        newTodo.classList.add("personalcolor");
+    } else if(categorySelected.value === "home"){
+        newTodo.classList.add("homecolor");
+    } else if(categorySelected.value === "work"){
+        newTodo.classList.add("workcolor");
+    }
+    TodosList.appendChild(newTodo);
 }
+personal.addEventListener("click", function(){
+    categoryHeader.innerText = "personal todos";
+    var lis = document.querySelectorAll("li");
+    for(var i = 0; i < lis.length; i++){
+        if (!(lis[i].innerHTML.includes('personal=""'))){
+             lis[i].style.display = "none";
+        }else{lis[i].style.display = "block";}
+    }
+});
+home.addEventListener("click", function(){
+    categoryHeader.innerText = "home todos";
+    var lis = document.querySelectorAll("li");
+    for(var i = 0; i < lis.length; i++){
+        if (!(lis[i].innerHTML.includes('home=""'))){
+            lis[i].style.display = "none";
+        }else{lis[i].style.display = "block";}
+    }
+});
+work.addEventListener("click", function(){
+    categoryHeader.innerText = "work todos";
+    var lis = document.querySelectorAll("li");
+    for(var i = 0; i < lis.length; i++){
+        if (!(lis[i].innerHTML.includes('work=""'))){
+             lis[i].style.display = "none";
+        } else{lis[i].style.display = "block";}
+    }
+});
+all.addEventListener("click", function(){
+    categoryHeader.innerText = "all todos";
+    var lis = document.querySelectorAll("li");
+    for(var i = 0; i < lis.length; i++){
+        lis[i].style.display = "block";
+    }
+});
+//Day15 task
+TodosList.addEventListener("click", deleted);
+function deleted(e){
+    if(e.target.classList.contains('delete')){
+        if(confirm('Are You Sure?')){
+          var li = e.target.parentElement;
+          TodosList.removeChild(li);
+        }
+       }
+}
+//
+
+
+
 
