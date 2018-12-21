ffb2a618f830fd4c24524cfa7e3371fa7fcd967a
Olamide <aboyejiolamide15@gmail.com>
2018-12-21 13:55:19 +0100

    Rounding up

--- aboyeji.olamide-aboyejiolamide15/js/schema.js
+++ aboyeji.olamide-aboyejiolamide15/js/schema.js
@@ -1,4 +1,4 @@
-//Declare all variables
+//Declare variables
 var button = document.querySelector("button");
 var closeButton = document.getElementById("closeButton");
 var newTodoBlock = document.getElementsByClassName("newTodoBlock")[0];
@@ -11,9 +11,13 @@ var personal = document.getElementById("personalNav");
 var work = document.getElementById("workNav");
 var home = document.getElementById("homeNav");
 var TodosList = document.getElementById("TodosList");
+var logout = document.getElementById("logout");
 //Day 12 task
 var todoDeadlineDate = document.querySelector("input[type=date]");
 var todoDeadlineTime =  document.querySelector("input[type=time]");
+//
+var allDeadlines = document.getElementsByClassName("deadline");
+var todoTitle = document.getElementsByClassName("todoTitle");
 //
 button.addEventListener("click", addTodo);
 form.addEventListener("submit", formSubmit);
@@ -25,28 +29,47 @@ function addTodo(){
     newTodoBlock.classList.add("newTodoBlockActive");
     button.style.display = "none";
 }
-
-function formSubmit(e){
-    e.preventDefault();
-    newTodoBlock.classList.remove("newTodoBlockActive");
-    button.style.display = "initial";
-    var newTodo = document.createElement("li");
-    newTodo.innerHTML = todoText.value + "<span class=hide " + categorySelected.value + "></span>" + "<span title='Remove Todo' class='delete'>&#128465;</span><br>" + 
-    todoDeadlineDate.value + " "+ todoDeadlineTime.value;
-    if (categorySelected.value === "personal"){
-        newTodo.classList.add("personalcolor");
-    } else if(categorySelected.value === "home"){
-        newTodo.classList.add("homecolor");
-    } else if(categorySelected.value === "work"){
-        newTodo.classList.add("workcolor");
+//Confirm if user want to Log out
+logout.addEventListener("click", function(e){
+    if(!(confirm("Are you sure you want to Logout?"))){
+        e.preventDefault();
     }
-    TodosList.appendChild(newTodo);
+})
+//What to do when todo form is submitted
+function formSubmit(e){
+     e.preventDefault();
+    var currentTimeDigits = currentTime();
+    var todoTime = new Date(todoDeadlineDate.value + " " + todoDeadlineTime.value);
+    var todoTimeDigits = Date.parse(todoTime);
+    if(currentTimeDigits >= todoTimeDigits){
+        //Alert Error messsage if user selected an earlier date as deadline for todo
+       alert("Enter a future date");  
+    }else{
+        newTodoBlock.classList.remove("newTodoBlockActive");
+        button.style.display = "initial";
+        var newTodo = document.createElement("li"); //Create a new list element
+        newTodo.innerHTML = "<span class='todoTitle'>"+ todoText.value +"</span>" +"<span title='Remove Todo' class='delete'>&#128465;</span><span class='complete' title='Completed'>&#10004;</span><br>" + 
+        "<span class='deadline'>" + todoDeadlineDate.value +" "+ todoDeadlineTime.value + "</span>"; //Input values from form into list element
+        //Check category selected to determine todo background color
+        if (categorySelected.value === "personal"){
+            newTodo.classList.add("personalcolor");
+        } else if(categorySelected.value === "home"){
+            newTodo.classList.add("homecolor");
+        } else if(categorySelected.value === "work"){
+            newTodo.classList.add("workcolor");
+        }
+        //Append the new list(li) element into its parent Element(ul) already on the page
+        TodosList.appendChild(newTodo);  
+        //Clear form values
+        todoText.value = "", todoDeadlineDate.value = "", todoDeadlineTime.value = ""; 
+    }
 }
+//Ensuring only todos on a particular category are displayed when category is clicked
 personal.addEventListener("click", function(){
     categoryHeader.innerText = "personal todos";
     var lis = document.querySelectorAll("li");
     for(var i = 0; i < lis.length; i++){
-        if (!(lis[i].innerHTML.includes('personal=""'))){
+        if (!(lis[i].classList.contains("personalcolor"))){
              lis[i].style.display = "none";
         }else{lis[i].style.display = "block";}
     }
@@ -55,7 +78,7 @@ home.addEventListener("click", function(){
     categoryHeader.innerText = "home todos";
     var lis = document.querySelectorAll("li");
     for(var i = 0; i < lis.length; i++){
-        if (!(lis[i].innerHTML.includes('home=""'))){
+        if (!(lis[i].classList.contains("homecolor"))){
             lis[i].style.display = "none";
         }else{lis[i].style.display = "block";}
     }
@@ -64,7 +87,7 @@ work.addEventListener("click", function(){
     categoryHeader.innerText = "work todos";
     var lis = document.querySelectorAll("li");
     for(var i = 0; i < lis.length; i++){
-        if (!(lis[i].innerHTML.includes('work=""'))){
+        if (!(lis[i].classList.contains("workcolor"))){
              lis[i].style.display = "none";
         } else{lis[i].style.display = "block";}
     }
@@ -76,18 +99,46 @@ all.addEventListener("click", function(){
         lis[i].style.display = "block";
     }
 });
-//Day15 task
+//
+//Day15 task 
+//Deleting and Completing Todos
 TodosList.addEventListener("click", deleted);
 function deleted(e){
     if(e.target.classList.contains('delete')){
-        if(confirm('Are You Sure?')){
-          var li = e.target.parentElement;
-          TodosList.removeChild(li);
+        if(confirm('Are You Sure you want to Remove this Todo?')){
+            var li = e.target.parentElement;
+            TodosList.removeChild(li);
         }
-       }
+    }else if(e.target.classList.contains("complete")){
+        if(confirm('Are you Sure?')){
+            alert("Nice work Completing your todo\nKeep it up!");
+            var li = e.target.parentElement;
+            TodosList.removeChild(li);
+        }
+    } 
 }
-//
-
-
-
-
+//Get Current Time
+function currentTime(){
+    var currentTime = new Date();
+    var currentTimeDigits = Date.parse(currentTime);
+    return currentTimeDigits;
+}
+function overdueAlert(){
+    for(var i = 0; i < allDeadlines.length; i++){
+        var currentTimeDigits = currentTime();
+        var todoTime = new Date(allDeadlines[i].innerText);
+        var todoTimeDigits = Date.parse(todoTime);
+        //Compare Todo Deadline with Current Time
+        if(currentTimeDigits >= todoTimeDigits){
+            //Change Todo Background Color to Red and text Color to white if Deadline has been met
+            todoTitle[i].parentNode.style.backgroundColor = "red";
+            todoTitle[i].parentNode.style.color = "white";
+            todoTitle[i].nextSibling.style.color = "white";
+            //alert a message saying a particular todo is overdue
+            alert(todoTitle[i].innerText + " is overdue");
+        }
+    }
+}
+setInterval(currentTime,1000);
+setInterval(overdueAlert,30000);
+overdueAlert();
